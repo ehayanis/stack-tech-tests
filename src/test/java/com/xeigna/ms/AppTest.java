9d1faf06d48b33a9848d97c0097aa7a455261840
ehayanis <hayani.mehdi149@gmail.com>
2017-09-10 21:52:04 +0200

    First Selenium Test Case

--- src/test/java/com/xeigna/ms/AppTest.java
+++ src/test/java/com/xeigna/ms/AppTest.java
@@ -1,38 +1,70 @@
 package com.xeigna.ms;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class AppTest {
+    private String baseUrl;
+    private WebDriver driver;
+    private ScreenshotHelper screenshotHelper;
+
+    @Before
+    public void openBrowser() {
+        URL url = null;
+        try {
+            url = new URL("http://localhost:4444/wd/hub");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        driver = new RemoteWebDriver(url, DesiredCapabilities.chrome());
+        baseUrl = "https://www.google.com";
+        driver.get(baseUrl);
+        screenshotHelper = new ScreenshotHelper();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @After
+    public void saveScreenshotAndCloseBrowser() throws IOException {
+        screenshotHelper.saveScreenshot("screenshot.png");
+        driver.quit();
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void searchForSelenium() throws IOException {
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys("Selenium!");
+        searchField.submit();
+        /* assertTrue("The page title should start with the search string after the search.",
+                (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+                    public Boolean apply(WebDriver d) {
+                        return d.getTitle().toLowerCase().startsWith("drupal!");
+                    }
+                })
+        ); */
+    }
+
+    private class ScreenshotHelper {
+
+        public void saveScreenshot(String screenshotFileName) throws IOException {
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(screenshot, new File(screenshotFileName));
+        }
     }
 }
